7ecd70431958ecfa4b176ba525c24031e222b7b2
Hua Zan Mingz <phamquangminhex@gmail.com>
2023-07-30 22:02:45 +0700

    - Update build

--- index.1f493.js
+++ index.1f493.js
@@ -1,4 +1,4 @@
-System.register(["./application.07abb.js"], function (_export, _context) {
+System.register(["./application.e5438.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.07abb.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application07abbJs) {
-      createApplication = _application07abbJs.createApplication;
+    setters: [function (_applicationE5438Js) {
+      createApplication = _applicationE5438Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
